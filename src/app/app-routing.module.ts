0c383d72c959f85582695506c287c8270fbec894
rubensanzrosa <ruben.sanz@cieep.com>
2023-01-20 12:17:15 +0100

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {EstadisticasComponent} from "./pages/estadisticas/estadisticas.component";
+import {ArticulosComponent} from "./pages/articulos/articulos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'estadisticas', component: EstadisticasComponent },
+  {path: 'articulos', component: ArticulosComponent},
+  {path: '', redirectTo:'/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { ArticulosComponent } from './pages/articulos/articulos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    EstadisticasComponent,
+    ArticulosComponent
   ],
   imports: [
     BrowserModule,
